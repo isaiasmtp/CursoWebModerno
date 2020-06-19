26911d9e856e1cd354bce93e48b4996aa6156abc
isaiasmtp <isaias.pontes@ccc.ufcg.edu.br>
2020-06-19 08:58:35 -0300

    Atualizando Arquivos

---
+++ esnext/set.js
@@ -0,0 +1,19 @@
+// nao aceita repetição e nao é indexada
+
+const times = new Set()
+
+times.add('Vasco')
+times.add('São Paulo').add('Palmeiras').add('Corinthias')
+times.add('Flamengo')
+times.add('Vasco')
+
+console.log(times)
+console.log(times.size)
+console.log(times.has('Vasco'))
+times.delete('Flamengo')
+console.log(times.has('Flamengo'));
+
+const nomes = ['Raquel', 'Lucas', 'Julia', 'Lucas']
+
+const nomesSet = new Set(nomes)
+console.log(nomesSet);
